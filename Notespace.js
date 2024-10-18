0c455ae6c64d9a33859868a7352a5f2d317b1122
Demitri <demitrilucienclark@gmail.com>
2024-10-18 00:25:16 -0400

    button works

--- Notespace.js
+++ Notespace.js
@@ -1,10 +1,71 @@
-
-const EmptyWorkspace = document.getElementById("emptyWorkspace");
-const SplineView1 = document.getElementById("spacer1");
-
 const dropdownButton = document.getElementById('dropdownButton');
 const dropdownContent = document.getElementById('dropdownContent');
 const chevron = document.querySelector('.chevron');
+const workspace = document.getElementById('workspace');
+
+
+
+function openUserNotes(){
+
+    if(!(workspace.classList.contains('myNotes'))){
+        while(workspace.firstChild){
+            workspace.removeChild(workspace.firstChild);
+        }
+    }
+
+    myNotes = document.createElement('div');
+    myNotes.classList.add('myNotes'); 
+
+    myNotesLister = document.createElement('div');
+    myNotesLister.classList.add('myNotesLister'); 
+
+    // Eventually add server component for adding each note but for now im doing this
+    for(let i = 0; i<30; i++){
+        noteCard = document.createElement('div');
+        noteCard.classList.add('noteCard'); 
+
+        noteName = document.createElement('p');
+        noteName.classList.add('noteName'); 
+
+        noteDate = document.createElement('p');
+        noteDate.classList.add('noteDate'); 
+
+        noteCard.append(noteName);
+        noteCard.append(noteDate);
+
+        myNotesLister.append(noteCard);
+    }
+
+    myNotes.append(myNotesLister);
+    workspace.append(myNotes);
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------
+
 
 dropdownButton.addEventListener('click', function(event) {
     event.stopPropagation();
@@ -15,14 +76,5 @@ dropdownButton.addEventListener('click', function(event) {
 dropdownContent.addEventListener('click', function(event) {
        event.stopPropagation();
     });
-    
 
-function EmptyCreateNote(){
-    EmptyWorkspace.style.visibility = "hidden";
-    SplineView1.style.height = "0px";
-}
-
-function ViewEmptyWorkspace(){
-    EmptyWorkspace.style.visibility = "visible";
-    SplineView1.style.height = "360px";
-}
+// ---------------------------------------------------------------------------------------------------------------------------------------------
